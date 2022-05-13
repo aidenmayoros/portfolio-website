220dced15e52f3bbeaabe0980ff0f1349163cfd6
Aiden Mayoros <aiden.mayoros@gmail.com>
2022-05-13 16:15:52 -0700

    refactored main page and NAV

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,29 @@
 import "../styles/HomePage.css";
 import { Avatar } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { Box } from "@mui/system";
 
 export default function HomePage() {
 	return (
-		<div className='home-container'>
-			<div className='homePage-maintext'>
+		<Box className='home-container'>
+			<Box mt={15} ml={10}>
 				<Avatar
 					alt='Aiden Mayoros'
 					src='/images/aiden-avatar.jpg'
 					sx={{ width: 80, height: 80 }}
 				/>
-				<h1 id='homePage-greeting'>Aiden Mayoros</h1>
-				<h2>
+				<Typography variant='h2'>Aiden Mayoros</Typography>
+				<Typography variant='h2' fontSize='2em'>
 					Jr. Software Engineer and <br /> Web Developer
-				</h2>
-				<h3>
+				</Typography>
+				<Typography
+					variant='h4'
+					fontWeight='300'
+					sx={{ fontSize: "1.2em", mt: 5 }}>
 					Check out some of the awesome
 					<br /> projects I've made!
-				</h3>
-			</div>
-		</div>
+				</Typography>
+			</Box>
+		</Box>
 	);
 }
