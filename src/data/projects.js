1a2d2b2fa754ae0aeec35f22eb0c3d1cc90343d5
Aiden Mayoros <aiden.mayoros@gmail.com>
2022-08-14 21:49:02 -0700

    updated all website links

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,81 +1,79 @@
 export default [
 	{
-		projectName: "Weather Forecast API",
-		img: "/images/weather-forecast-preview.png",
-		pathLink: "/weather_forecast",
+		projectName: 'Weather Forecast API',
+		img: '/images/weather-forecast-preview.png',
+		pathLink: '/weather_forecast',
 		challenges:
 			"With this project it was challenging to get the API working since I was fairly new to asynchronous Javascript. I used OpenWeatherMap's free Weather API and had to spend a good amount of time looking through the documentation to figure out specifically how it works. Once I sifted through documentation, I eventually got it working.",
 		knowledgeLearned:
-			"The main thing I learned from this project is how important documentation is to read & understand. In this project I also learned how to use the built-in javascript map prototype method to create & return an array of components, each representing a weather day of the week.",
-		technologiesUsed: "React | Bootstrap | OpenWeatherMap API",
-		appLink:
-			"https://aiden-mayoros-website.herokuapp.com/weather_forecast/app/",
+			'The main thing I learned from this project is how important documentation is to read & understand. In this project I also learned how to use the built-in javascript map prototype method to create & return an array of components, each representing a weather day of the week.',
+		technologiesUsed: 'React | Bootstrap | OpenWeatherMap API',
+		appLink: 'aidenmayoros.com/weather_forecast/app',
 	},
 	{
-		projectName: "Image Carousel",
-		img: "/images/image-carousel-preview.png",
-		pathLink: "/image_carousel",
+		projectName: 'Image Carousel',
+		img: '/images/image-carousel-preview.png',
+		pathLink: '/image_carousel',
 		challenges:
-			"With this project I was fairly new to Javascript and wanted to learn something I had frequently seen on websites. This project was challenging for me since I was pretty new to CSS. I also grew a deeper understanding of setInterval so the carousel transitions through images every few seconds.",
+			'With this project I was fairly new to Javascript and wanted to learn something I had frequently seen on websites. This project was challenging for me since I was pretty new to CSS. I also grew a deeper understanding of setInterval so the carousel transitions through images every few seconds.',
 		knowledgeLearned:
 			"A key point I learned from this project was that using built-in Javascript methods made my code a lot nicer and easier to read & digest. This project was one of the first projects where I had to use HTML, CSS, and Javascript to query the DOM. I now understand the importance of using them, as they are just shortcuts to common programming tasks. I also learned that if I'm struggling with a specific issue, there are a lot of online resources & smart people out there that that I can refer to.",
-		technologiesUsed: "HTML | CSS | JavaScript",
-		appLink: "https://aiden-mayoros-website.herokuapp.com/image_carousel/app/",
+		technologiesUsed: 'HTML | CSS | JavaScript',
+		appLink: 'aidenmayoros.com/image_carousel/app',
 	},
 	{
-		projectName: "DnD Character Website",
-		img: "/images/dndPreview.png",
-		pathLink: "/dnd_character",
+		projectName: 'DnD Character Website',
+		img: '/images/dndPreview.png',
+		pathLink: '/dnd_character',
 		challenges:
-			"This was one of the first multi-page web apps I made with React. One of the challenges here was using React Router to route to different components with React. I was also challenged with some of the CSS I wanted like the full image as the background. One feature I really wanted to do was a flip card affect. This proved to be one of the hardest parts of the project but once I got it working I was very pleased with it.",
+			'This was one of the first multi-page web apps I made with React. One of the challenges here was using React Router to route to different components with React. I was also challenged with some of the CSS I wanted like the full image as the background. One feature I really wanted to do was a flip card affect. This proved to be one of the hardest parts of the project but once I got it working I was very pleased with it.',
 		knowledgeLearned:
-			"I learned in this project how to use React Router to switch between components based on URL routes which I ended up using in my later React projects. This project also introduced me to custom hooks. Up to this point, I was mainly using the useState and useEffect hooks, but this project introduced the useNavigation hook to route between components. I also learned that there are a lot of CSS effects that are already out there to use, and you do not have to make them yourself from scratch. I had found the CSS flip card effect online and was able to use it and edit it to my liking.",
-		technologiesUsed: "React | React Router",
-		appLink: "https://aiden-mayoros-website.herokuapp.com/dnd_character/app/",
+			'I learned in this project how to use React Router to switch between components based on URL routes which I ended up using in my later React projects. This project also introduced me to custom hooks. Up to this point, I was mainly using the useState and useEffect hooks, but this project introduced the useNavigation hook to route between components. I also learned that there are a lot of CSS effects that are already out there to use, and you do not have to make them yourself from scratch. I had found the CSS flip card effect online and was able to use it and edit it to my liking.',
+		technologiesUsed: 'React | React Router',
+		appLink: 'aidenmayoros.com/dnd_character/app',
 	},
 	{
-		projectName: "Simple Calculator",
-		img: "/images/calculator-preview.png",
-		pathLink: "/calculator",
+		projectName: 'Simple Calculator',
+		img: '/images/calculator-preview.png',
+		pathLink: '/calculator',
 		challenges:
 			"Prior to this project, I had created a basic Mortgage Calculator, but this time I decided to take on the challenge to create something similar with React. A big challenge to this project was learning about React's component lifecycle and how components re-render on state change. I ran into many errors and infinite render loops which eventually led to a better understandin of React component lifecycles. I also learned a lot about passing down props to components and how that entire lifecycle works as well.",
 		knowledgeLearned:
-			"I learned how to structure components in a project and organize them all in a larger project. Learned how to use state more and passing state down to other components as props. This project helped me a lot put all the pieces of React together into a project and really see the benefits of using React and state over just querying the DOM directly with vanilla Javascript.",
-		technologiesUsed: "React",
-		appLink: "https://aiden-mayoros-website.herokuapp.com/calculator/app/",
+			'I learned how to structure components in a project and organize them all in a larger project. Learned how to use state more and passing state down to other components as props. This project helped me a lot put all the pieces of React together into a project and really see the benefits of using React and state over just querying the DOM directly with vanilla Javascript.',
+		technologiesUsed: 'React',
+		appLink: 'aidenmayoros.com/calculator/app',
 	},
 	{
-		projectName: "Mortgage Calculator with JavaScript",
-		img: "/images/mortgage-calculator-preview.png",
-		pathLink: "/mortgage_calculator",
+		projectName: 'Mortgage Calculator with JavaScript',
+		img: '/images/mortgage-calculator-preview.png',
+		pathLink: '/mortgage_calculator',
 		challenges:
-			"I decided to do this project because I was working in the mortgage industry and it seemed like a project that would help me be more efficient at my job to quickly calculte mortgage payments. I was still really new and learning and so learning how to write functions to grab text input data and compute formulas to calculate was all pretty new. A big challenge with this project was querying and manipulating the DOM to add and update DOM elements",
+			'I decided to do this project because I was working in the mortgage industry and it seemed like a project that would help me be more efficient at my job to quickly calculte mortgage payments. I was still really new and learning and so learning how to write functions to grab text input data and compute formulas to calculate was all pretty new. A big challenge with this project was querying and manipulating the DOM to add and update DOM elements',
 		knowledgeLearned:
-			"I learned how to manipulate the DOM via JavaScript and the document object and writing functions with math equations to compute data. I also learned how to use conditional statements for form validation.",
-		technologiesUsed: "HTML | CSS | JavaScript",
-		appLink:
-			"https://aiden-mayoros-website.herokuapp.com/mortgage_calculator/app/",
+			'I learned how to manipulate the DOM via JavaScript and the document object and writing functions with math equations to compute data. I also learned how to use conditional statements for form validation.',
+		technologiesUsed: 'HTML | CSS | JavaScript',
+		appLink: 'aidenmayoros.com/mortgage_calculator/app',
 	},
 	{
-		projectName: "Tic Tac Toe with JavaScript",
-		img: "/images/tic-tac-toe-preview.png",
-		pathLink: "/tic-tac-toe",
+		projectName: 'Tic Tac Toe with JavaScript',
+		img: '/images/tic-tac-toe-preview.png',
+		pathLink: '/tic-tac-toe',
 		challenges:
-			"Tic Tac Toe was one of the hardest projects for me and took a lot of time. I thought it would be fun to do and did not realize the extent of how much work it would be to make a game like this. It was difficult to make the flow of the game, determine winners, and create the computer playing. This project pushed me a lot when working on it to learn how to use methods, objects, arrays, and validation. This project challenged everything I had learned from previous and much more.",
+			'Tic Tac Toe was one of the hardest projects for me and took a lot of time. I thought it would be fun to do and did not realize the extent of how much work it would be to make a game like this. It was difficult to make the flow of the game, determine winners, and create the computer playing. This project pushed me a lot when working on it to learn how to use methods, objects, arrays, and validation. This project challenged everything I had learned from previous and much more.',
 		knowledgeLearned:
-			"I learned the importance of keeping functions simple, and not stuffing too much logic into a single function. I seperated business logic into smaller functions and composed them together, which led to more readable & understandable code.",
-		technologiesUsed: "HTML | CSS | JavaScript",
-		appLink: "https://aiden-mayoros-website.herokuapp.com/tic-tac-toe/app/",
+			'I learned the importance of keeping functions simple, and not stuffing too much logic into a single function. I seperated business logic into smaller functions and composed them together, which led to more readable & understandable code.',
+		technologiesUsed: 'HTML | CSS | JavaScript',
+		appLink: 'aidenmayoros.com/tic-tac-toe/app',
 	},
 	{
-		projectName: "World Clock",
-		img: "/images/clock-preview.png",
-		pathLink: "/world_clock",
+		projectName: 'World Clock',
+		img: '/images/clock-preview.png',
+		pathLink: '/world_clock',
 		challenges:
 			"This project challenged me to learn Javascript's built-in async/await, I did not entirely understand exactly how promises worked before this project, but had a firm understanding after completion. It was a struggle at the start to understand how Javascript deals with asyncronous code, and challenged me to use async/await to grab data from OpenWeatherMap's public API.",
 		knowledgeLearned:
 			"I learned async/await and developed a knowledge of JavaScript promisses. This project also allowed me to deepen my knowledge of using API's.",
-		technologiesUsed: "HTML | CSS | JavaScript | OpenWeatherMap API",
-		appLink: "https://aiden-mayoros-website.herokuapp.com/world_clock/app/",
+		technologiesUsed: 'HTML | CSS | JavaScript | OpenWeatherMap API',
+		appLink: 'aidenmayoros.com/world_clock/app',
 	},
 ];
