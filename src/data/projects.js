24bdb256247c20d7d5e704d437cbeae94bb007d1
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-10-31 17:13:28 -0700

    Commented out some old projects to take them down

--- src/data/projects.js
+++ src/data/projects.js
@@ -80,17 +80,18 @@ export default [
 		technologiesUsed: 'React | Bootstrap | OpenWeatherMap API',
 		appLink: '/weather_forecast/app',
 	},
-	{
-		projectName: 'DnD Character Website',
-		img: '/images/dndPreview.png',
-		pathLink: '/dnd_character',
-		challenges:
-			'This was one of the first multi-page web apps I made with React. One of the challenges here was using React Router to route to different components with React. I was also challenged with some of the CSS I wanted like the full image as the background. One feature I really wanted to do was a flip card affect. This proved to be one of the hardest parts of the project but once I got it working I was very pleased with it.',
-		knowledgeLearned:
-			'I learned in this project how to use React Router to switch between components based on URL routes which I ended up using in my later React projects. This project also introduced me to custom hooks. Up to this point, I was mainly using the useState and useEffect hooks, but this project introduced the useNavigation hook to route between components. I also learned that there are a lot of CSS effects that are already out there to use, and you do not have to make them yourself from scratch. I had found the CSS flip card effect online and was able to use it and edit it to my liking.',
-		technologiesUsed: 'React | React Router',
-		appLink: '/dnd_character/app',
-	},
+	// This is an old project that is not good enough to show anymore
+	// {
+	// 	projectName: 'DnD Character Website',
+	// 	img: '/images/dndPreview.png',
+	// 	pathLink: '/dnd_character',
+	// 	challenges:
+	// 		'This was one of the first multi-page web apps I made with React. One of the challenges here was using React Router to route to different components with React. I was also challenged with some of the CSS I wanted like the full image as the background. One feature I really wanted to do was a flip card affect. This proved to be one of the hardest parts of the project but once I got it working I was very pleased with it.',
+	// 	knowledgeLearned:
+	// 		'I learned in this project how to use React Router to switch between components based on URL routes which I ended up using in my later React projects. This project also introduced me to custom hooks. Up to this point, I was mainly using the useState and useEffect hooks, but this project introduced the useNavigation hook to route between components. I also learned that there are a lot of CSS effects that are already out there to use, and you do not have to make them yourself from scratch. I had found the CSS flip card effect online and was able to use it and edit it to my liking.',
+	// 	technologiesUsed: 'React | React Router',
+	// 	appLink: '/dnd_character/app',
+	// },
 	{
 		projectName: 'Simple Calculator',
 		img: '/images/calculator-preview.png',
@@ -102,37 +103,40 @@ export default [
 		technologiesUsed: 'React',
 		appLink: '/calculator/app',
 	},
-	{
-		projectName: 'Tic Tac Toe with JavaScript',
-		img: '/images/tic-tac-toe-preview.png',
-		pathLink: '/tic-tac-toe',
-		challenges:
-			'Tic Tac Toe was one of the hardest projects for me and took a lot of time. I thought it would be fun to do and did not realize the extent of how much work it would be to make a game like this. It was difficult to make the flow of the game, determine winners, and create the computer playing. This project pushed me a lot when working on it to learn how to use methods, objects, arrays, and validation. This project challenged everything I had learned from previous and much more.',
-		knowledgeLearned:
-			'I learned the importance of keeping functions simple, and not stuffing too much logic into a single function. I seperated business logic into smaller functions and composed them together, which led to more readable & understandable code.',
-		technologiesUsed: 'HTML | CSS | JavaScript',
-		appLink: '/tic-tac-toe/app',
-	},
-	{
-		projectName: 'Mortgage Calculator with JavaScript',
-		img: '/images/mortgage-calculator-preview.png',
-		pathLink: '/mortgage_calculator',
-		challenges:
-			'I decided to do this project because I was working in the mortgage industry and it seemed like a project that would help me be more efficient at my job to quickly calculte mortgage payments. I was still really new and learning and so learning how to write functions to grab text input data and compute formulas to calculate was all pretty new. A big challenge with this project was querying and manipulating the DOM to add and update DOM elements',
-		knowledgeLearned:
-			'I learned how to manipulate the DOM via JavaScript and the document object and writing functions with math equations to compute data. I also learned how to use conditional statements for form validation.',
-		technologiesUsed: 'HTML | CSS | JavaScript',
-		appLink: '/mortgage_calculator/app',
-	},
-	{
-		projectName: 'Image Carousel',
-		img: '/images/image-carousel-preview.png',
-		pathLink: '/image_carousel',
-		challenges:
-			'With this project I was fairly new to Javascript and wanted to learn something I had frequently seen on websites. This project was challenging for me since I was pretty new to CSS. I also grew a deeper understanding of setInterval so the carousel transitions through images every few seconds.',
-		knowledgeLearned:
-			"A key point I learned from this project was that using built-in Javascript methods made my code a lot nicer and easier to read & digest. This project was one of the first projects where I had to use HTML, CSS, and Javascript to query the DOM. I now understand the importance of using them, as they are just shortcuts to common programming tasks. I also learned that if I'm struggling with a specific issue, there are a lot of online resources & smart people out there that that I can refer to.",
-		technologiesUsed: 'HTML | CSS | JavaScript',
-		appLink: '/image_carousel/app',
-	},
+	// This is an old project that is not good enough to show anymore
+	// {
+	// 	projectName: 'Tic Tac Toe with JavaScript',
+	// 	img: '/images/tic-tac-toe-preview.png',
+	// 	pathLink: '/tic-tac-toe',
+	// 	challenges:
+	// 		'Tic Tac Toe was one of the hardest projects for me and took a lot of time. I thought it would be fun to do and did not realize the extent of how much work it would be to make a game like this. It was difficult to make the flow of the game, determine winners, and create the computer playing. This project pushed me a lot when working on it to learn how to use methods, objects, arrays, and validation. This project challenged everything I had learned from previous and much more.',
+	// 	knowledgeLearned:
+	// 		'I learned the importance of keeping functions simple, and not stuffing too much logic into a single function. I seperated business logic into smaller functions and composed them together, which led to more readable & understandable code.',
+	// 	technologiesUsed: 'HTML | CSS | JavaScript',
+	// 	appLink: '/tic-tac-toe/app',
+	// },
+	// This is an old project that is not good enough to show anymore
+	// {
+	// 	projectName: 'Mortgage Calculator with JavaScript',
+	// 	img: '/images/mortgage-calculator-preview.png',
+	// 	pathLink: '/mortgage_calculator',
+	// 	challenges:
+	// 		'I decided to do this project because I was working in the mortgage industry and it seemed like a project that would help me be more efficient at my job to quickly calculte mortgage payments. I was still really new and learning and so learning how to write functions to grab text input data and compute formulas to calculate was all pretty new. A big challenge with this project was querying and manipulating the DOM to add and update DOM elements',
+	// 	knowledgeLearned:
+	// 		'I learned how to manipulate the DOM via JavaScript and the document object and writing functions with math equations to compute data. I also learned how to use conditional statements for form validation.',
+	// 	technologiesUsed: 'HTML | CSS | JavaScript',
+	// 	appLink: '/mortgage_calculator/app',
+	// },
+	// This is an old project that is not good enough to show anymore
+	// {
+	// 	projectName: 'Image Carousel',
+	// 	img: '/images/image-carousel-preview.png',
+	// 	pathLink: '/image_carousel',
+	// 	challenges:
+	// 		'With this project I was fairly new to Javascript and wanted to learn something I had frequently seen on websites. This project was challenging for me since I was pretty new to CSS. I also grew a deeper understanding of setInterval so the carousel transitions through images every few seconds.',
+	// 	knowledgeLearned:
+	// 		"A key point I learned from this project was that using built-in Javascript methods made my code a lot nicer and easier to read & digest. This project was one of the first projects where I had to use HTML, CSS, and Javascript to query the DOM. I now understand the importance of using them, as they are just shortcuts to common programming tasks. I also learned that if I'm struggling with a specific issue, there are a lot of online resources & smart people out there that that I can refer to.",
+	// 	technologiesUsed: 'HTML | CSS | JavaScript',
+	// 	appLink: '/image_carousel/app',
+	// },
 ];
