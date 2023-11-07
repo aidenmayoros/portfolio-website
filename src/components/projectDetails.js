ef9ba41781a02e2864618ab81727bfd8ad7a2016
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-11-07 14:44:19 -0700

    changed fade in for project details

--- src/components/projectDetails.js
+++ src/components/projectDetails.js
@@ -9,10 +9,16 @@ export default function ProjectDetails({ project }) {
 		<Box className='project-details-container'>
 			<Box className='project-details'>
 				<Fade left duration={1000}>
-					<Typography sx={{ mt: 3, mb: 2 }} variant='h2' id='project-details-h2'>
+					<Typography
+						sx={{ mt: 3, mb: 2 }}
+						variant='h2'
+						id='project-details-h2'>
 						{project.projectName}
 					</Typography>
-					<img className='project-page-images' src={project.img} alt='project preview'></img>
+					<img
+						className='project-page-images'
+						src={project.img}
+						alt='project preview'></img>
 					<Button
 						sx={{ mt: 2 }}
 						variant='contained'
@@ -29,23 +35,29 @@ export default function ProjectDetails({ project }) {
 						fontWeight={300}>
 						Project challenges
 					</Typography>
-					<Typography sx={{ fontSize: 20, mt: 1 }}>{project.challenges}</Typography>
+					<Typography sx={{ fontSize: 20, mt: 1 }}>
+						{project.challenges}
+					</Typography>
 				</Fade>
-				<Fade left delay={2000}>
+				<Fade left delay={1000}>
 					<Typography
 						sx={{ mt: 5, mb: 1, textDecoration: 'underline' }}
 						variant='h4'
 						fontWeight={300}>
 						What I learned
 					</Typography>
-					<Typography sx={{ fontSize: 20 }}>{project.knowledgeLearned}</Typography>
+					<Typography sx={{ fontSize: 20 }}>
+						{project.knowledgeLearned}
+					</Typography>
 					<Typography
 						sx={{ mt: 5, mb: 1, textDecoration: 'underline' }}
 						variant='h4'
 						fontWeight={300}>
 						Technologies Used
 					</Typography>
-					<Typography sx={{ mb: 4, fontSize: 20 }}>{project.technologiesUsed}</Typography>
+					<Typography sx={{ mb: 4, fontSize: 20 }}>
+						{project.technologiesUsed}
+					</Typography>
 				</Fade>
 			</Box>
 		</Box>
