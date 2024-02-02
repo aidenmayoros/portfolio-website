b43bc426ba91662d50b35f45c2315f8809e2657e
Aiden Mayoros <aiden.mayoros@gmail.com>
2024-02-01 19:15:33 -0700

    added more about me to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,22 +19,29 @@ export default function HomePage() {
 						sx={{ width: 100, height: 100, mb: 3 }}
 					/>
 				</Fade>
-				<Fade left delay={800}>
-					<Typography variant='h4' fontWeight='200'>
-						Aiden Mayoros
-					</Typography>
-				</Fade>
-				<Fade left delay={1200}>
-					<Typography variant='h4' fontWeight='200'>
-						Full Stack Software Engineer and <br /> Web Developer
-					</Typography>
+				<Fade delay={800}>
+					<Box className='home-page-typography'>
+						<Typography variant='h4' fontWeight='200'>
+							Hello I'm Aiden Mayoros
+						</Typography>
+						<Typography
+							variant='h5'
+							fontWeight='200'
+							sx={{ maxWidth: { xs: 300, sm: 400, lg: 600 }, pt: 2 }}>
+							Web developer with a foundation in customer service. Proficient in
+							Javascript, CSS and HTML. Enjoys tackling intellectually
+							challenging tasks and crafting user-centered solutions to
+							problems. Has a keen enthusiasm for continuous learning and
+							staying updated on new skills and technologies to drive consistent
+							improvement.
+						</Typography>
+						<Typography variant='h5' fontWeight='200' sx={{ mt: 5 }}>
+							Check out some of the awesome
+							<br /> projects I've made!
+						</Typography>
+					</Box>
 				</Fade>
 				<Fade bottom delay={1600}>
-					<Typography variant='h5' fontWeight='200' sx={{ mt: 5 }}>
-						Check out some of the awesome
-						<br /> projects I've made!
-					</Typography>
-
 					<Box className='home-page-buttons'>
 						<Button variant='contained' onClick={() => navigate('/projects')}>
 							View Projects
